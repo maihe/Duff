23502c1511a2fe3a58fa5d76b6ec508ad2bea376
Maiara <maiaraoliveira.dejesus@gmail.com>
2018-12-16 23:21:26 -0200

    Projeto Duff

---
+++ src/main/java/com/ciclic/duff/DuffApplication.java
@@ -0,0 +1,14 @@
+package com.ciclic.duff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DuffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DuffApplication.class, args);
+	}
+
+}
+
